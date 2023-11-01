c508779b8d65a1989cf9a1653786e931885c7610
Kevin Tervoli <65734031+kevintervoli@users.noreply.github.com>
2023-11-01 20:25:54 +0100

    Initial commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentName = new string[] { "Kevin", "Jane", "Mark", "Emily", "David" };
+            return Ok(studentName);
+        
+        }
+
+    }
+}
